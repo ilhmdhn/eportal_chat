483401577c627da59b7f59034a7d7ba1466fcff1
Ilham Dohaan <doha39anilham@gmail.com>
2025-05-15 11:27:58 +0700

    zzz

--- src/controller/chat.js
+++ src/controller/chat.js
@@ -2,6 +2,7 @@ const userTable = require('../models/user');
 const chatTable = require('../models/chat');
 const response = require('../tool/response');
 const { Op } = require('sequelize');
+const chat = require('../models/chat');
 
 const getChat = (req, res) => {
     try {
@@ -26,7 +27,7 @@ const getUserChat = async (req, res) => {
                     { [Op.and]: [{ nip: target }, { target: user }] }
                 ]
             },
-            order: [['time', 'DESC']],
+            order: [['time', 'ASC']],
             limit: limit,
             offset: offset,
             raw: true
@@ -40,13 +41,31 @@ const getUserChat = async (req, res) => {
 
 const getUsers = async (req, res) => {
     try {
-        console.log(`LOLOLO`)
         const user = await userTable.findAll({
             raw: true
         });
         res.send(response(true, user));
     } catch (error) {
         console.error(error.message)
+        res.status(500).send(response(false, null, error.message));
+    }
+}
+
+const postChat = async (req, res) => {
+    try {
+        await chatTable.create({
+            nip: '',
+            target: '',
+            time: '',
+            content: '',
+            readed: '',
+            reply_from: '',
+            id: '',
+            media: '',
+        });
+        
+    } catch (err) {
+        console.error(err.message)
         res.status(500).send(response(false, null, error.message));
     }
 }
